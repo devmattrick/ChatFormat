954d968520175bae5eb78a17141bb08a0fb9e012
Zack Pollard <zackpollard@ymail.com>
2015-11-20 11:56:40 +0000

    Fix the default config file.

--- config.default.php
+++ config.default.php
@@ -2,4 +2,5 @@
 	define("DB_IP", "localhost");
 	define("DB_USER", "root"); //You probably shouldn't run as root in prod ;)
 	define("DB_PASS", "password");
-	define("DB_SCHMEA", "logs")
+	define("DB_SCHMEA", "logs");
+?>
